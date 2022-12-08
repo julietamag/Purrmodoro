835ef5f3d4dee94436069aff0ae83ced48ebab25
Julieta Magnago <julietamagnago@gmail.com>
2022-12-08 11:46:27 +0100

    fix audio error on mobile

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import cat_working from './work_cat.jpeg';
 
 function App() {
   const [displayTime, setDisplayTime] = useState(25 * 60);
-  const [breakTime, setBreakTime] = useState(3);
-  const [sessionTime, setSessionTime] = useState(5);
+  const [breakTime, setBreakTime] = useState(5 * 60);
+  const [sessionTime, setSessionTime] = useState(25 * 60);
   const [timerOn, setTimerOn] = useState(false);
   const [onBreak, setOnBreak] = useState(false);
   // eslint-disable-next-line no-unused-vars
